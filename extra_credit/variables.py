570cd5f4360f9d3e3b463f5cbbef00a2d9a94ece
Easton-Sherwood <easton.sherwood001@albright.edu>
2024-09-05 10:20:18 -0400

    extra_credit

---
+++ extra_credit/variables.py
@@ -0,0 +1,16 @@
+# Easton Sherwood
+# Date: 9/5/2024
+# This is the starter file for CSC141 lab day 3
+
+print("To generate your introduction, I will need some information from you...")
+name = input(f"What is your name? ")
+age = int(input(f"What is your age? "))
+major = input(f"What is your major? ")
+
+print(f"Hi, my name is {name} and I am {age} years old! At Albright college, I study {major}.")
+
+# I accidentally created my own message and didn't want to completely get rid of it
+#print(f"Hello {name}, you're majoring in {major} before you even turned {age+1}!")
+
+
+

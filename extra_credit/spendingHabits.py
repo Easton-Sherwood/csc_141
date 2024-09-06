ff726db3b0396d413d1d2d8bd52a7f8f44674102
Easton-Sherwood <easton.sherwood001@albright.edu>
2024-09-06 14:01:25 -0400

    extra_credit2

---
+++ extra_credit/spendingHabits.py
@@ -0,0 +1,17 @@
+# Easton Sherwood
+# Date: 9/6/2024
+# This is the starter file for CSC141 lab day 4
+
+PA_TAX = 0.06
+PRICE_OF_MEAL = 12.49
+
+
+
+print()
+print("| Lunch Spending Habits |")
+print("-------------------------")
+print(f"Tax per meal: {PRICE_OF_MEAL * PA_TAX}")
+print(f"Total without tax: {PRICE_OF_MEAL}")
+print(f"Total after tax: {PRICE_OF_MEAL * (PA_TAX + 1):.2f}")
+print("-------------------------")
+print()

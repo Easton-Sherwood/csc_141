203ef769b397c3d434f33b8b2bfaa3309ea0d0c8
Easton-Sherwood <easton.sherwood001@albright.edu>
2024-09-27 00:20:16 -0400

    fixes

--- 05_if_statements/05_11_ordinal_numbers.py
+++ 05_if_statements/05_11_ordinal_numbers.py
@@ -3,9 +3,9 @@ numbers = range(1, 10)
 for i in numbers:
     if int(i) == 1:
         print(f"{i}st")
-    if int(i) == 2:
+    elif int(i) == 2:
         print(f"{i}nd")
-    if int(i) == 3:
+    elif int(i) == 3:
         print(f"{i}rd")
-    if int(i) >= 4:
+    else:
         print(f"{i}th")

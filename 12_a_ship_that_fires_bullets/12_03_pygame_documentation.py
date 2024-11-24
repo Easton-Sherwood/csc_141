82ba6d7fc9c1b3e35352d84efbd1cfef8813704f
Easton-Sherwood <easton.sherwood001@albright.edu>
2024-11-23 19:11:34 -0500

    week 12

---
+++ 12_a_ship_that_fires_bullets/12_03_pygame_documentation.py
@@ -0,0 +1,5 @@
+"""
+The main page of pygame.org talks about bug fixes and changes
+they made for pygame. The most recent change was on june 25th, 2024
+and it also features some games created using pygames on the side
+"""
